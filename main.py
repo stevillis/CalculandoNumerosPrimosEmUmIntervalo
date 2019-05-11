048beb581c7bfb7bdd3b1ca50977ec23cfdc0739
Stévillis Monteiro de Sousa <smsstevillis@gmail.com>
2019-05-11 19:42:37 -0400

    Adicionada diferença entre os primos encontrados

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 __author__ = 'Stévillis Sousa'
+__copyright__ = "Copyright 2019, Calculando Números Primos em um Intervalo"
+__credits__ = ["Stévillis Sousa"]
+__license__ = "MIT"
+__version__ = "1.0.2"
+__maintainer__ = "Stévillis Sousa"
+__email__ = "stevillis@hotmail.com"
+__status__ = "Production"
 
-def calculaPrimo(numero):    
+
+def calculaPrimo(numero):
     divisores = 0
     if numero > 0:
         for divisor in range(1, numero):
             if numero % divisor == 0:
                 divisores += 1
-                if divisores > 1:            
+                if divisores > 1:
                     break
 
-        if divisores > 1 : # Número não é primo
+        if divisores > 1:  # Número não é primo
             return False
-        else: # Número é primo
+        else:  # Número é primo
             return numero
     else:
         print('Número preciso ser maior que zero!')
@@ -27,14 +35,23 @@ def rangePrimos(numero_inicial, numero_final):
     return lista_primos
 
 
+def diferencaPrimos(lista_primos):
+    print('\n\nDiferença entre os números primos encontrados:\n[', end=' ')
+    for cont, primo in enumerate(lista_primos):
+        if cont > 0:
+            print(primo - lista_primos[cont - 1], end=' ')
+    print(']')
+
+
 if __name__ == '__main__':
-    print('-'*60)
-    print('Este programa mostra a quantidade de números primos \nentre os intervalos dados a e b\n', '-'*60, '\n', sep='')
+    dashes = '-'*60
+    print(dashes)
+    print('Este programa mostra a quantidade de números primos \nentre os intervalos dados a e b\n', dashes, '\n', sep='')
 
     try:
         sair = 0
         while sair != -1:
-            
+
             a = int(input('Informe o intervalo inicial (a): '))
             b = int(input('Informe o intervalo inicial (b): '))
 
@@ -43,17 +60,17 @@ if __name__ == '__main__':
             else:
                 lista_primos = rangePrimos(a, b)
                 if len(lista_primos) > 0:
-                    print(f'\nExistem {len(lista_primos)} números primos entre {a} e {b}. \nEsses números são: ', end='')
+                    print(
+                        f'\nExistem {len(lista_primos)} números primos entre {a} e {b}. \nEsses números são: ', end='')
                     for cont, primo in enumerate(lista_primos):
                         if cont == len(lista_primos) - 1:
                             print('e', primo, end=' ')
                         else:
                             print(primo, end=' ')
-            
-            sair = int(input('\n\nDeseja definir outros intervalos? (-1 para sair): '))    
+
+                    diferencaPrimos(lista_primos)
+
+            sair = int(
+                input('\n\nDeseja definir outros intervalos? (-1 para sair): '))
     except ValueError:
-        print('Apenas números são aceitos!')
-        
-    
-
-
+        raise ValueError('Apenas números são aceitos!')
